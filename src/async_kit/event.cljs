95a558ac6c12eadc184ff97baf42692ae751c24f
megakorre <patrik.karlin@gmail.com>
2012-04-20 17:00:30 +0300

    some docs

--- src/async_kit/event.cljs
+++ src/async_kit/event.cljs
@@ -5,13 +5,17 @@
 (def ^{ :private true }
   event-handlers (atom {}))
 
-(defn await-event [event-name]
+(defn await-event 
+  "async wait for the next emit of a event"
+  [event-name]
   (async [s e]
          (if (@event-handlers event-name)
            (swap! event-handlers update-in [event-name] conj s)
            (swap! event-handlers assoc event-name [s]))))
 
-(defn emit [event-name data]
+(defn emit 
+  "emits a event to every one waiting for it"
+  [event-name data]
   (let [handlers (@event-handlers event-name)]
     (swap! event-handlers dissoc event-name)
     (doseq [handler handlers]
